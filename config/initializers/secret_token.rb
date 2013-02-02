20a71b8be868af378ac257dc792454d579cf4713
brad chaiken <brad.chaiken@gmail.com>
2013-02-02 17:46:52 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MHacks::Application.config.secret_token = 'a4f19217653e36ed82df27d6ce5661c67744538e35136ebd3ef4df3dde929d6313140b035f5cd3adf54a88e3ab87c8aed9c1e5dd7d7a9607c9502a8646b29694'
